e79a74791aaee2d08c4e31a79e72ffe1a46819d5
TayaHatum <21taya21@gmail.com>
2021-02-22 20:14:46 +0200

    Login in Pom

--- src/main/java/com/telran/pages/LoginPage.java
+++ src/main/java/com/telran/pages/LoginPage.java
@@ -1,10 +1,57 @@
 package com.telran.pages;
 
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
-    //userName
-    //password
-    //login
+import java.util.List;
+
+public class LoginPage extends PageBase{
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+    @FindBy(id="userName")
+    WebElement username;
+
+//    @FindBy(how = How.XPATH,using = "//*[@atributte='user']")
+//    WebElement name;
+//    @FindBy(css = ".claass")
+//    List<WebElement> list;
+
+      @FindBy (id="password")
+    WebElement pswd;
+    @FindBy (id="login")
+    WebElement loginBtn;
+
+
+    public ProfilePage login(String userName, String paswd){
+        type(username,userName);
+        type(pswd,paswd);
+
+        loginBtn.click();
+        return new ProfilePage(driver);
+    }
+    public LoginPage loginNegative(String userName, String paswd){
+        type(username,userName);
+        type(pswd,paswd);
+        loginBtn.click();
+        return this;
+    }
+    public LoginPage typeUser(String user){
+        type(username,user);
+        return this;
+    }
+    public LoginPage typePass(String paswd){
+        type(pswd,paswd);
+        return this;
+    }
+    public ProfilePage clickLogin(){
+        loginBtn.click();
+        return new ProfilePage(driver);
+    }
+
 
 }
--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,8 +1,19 @@
 package testcases;
 
+import com.telran.pages.LoginPage;
+import org.testng.annotations.Test;
+
 public class LoginTest extends TestBase{
     //hatum//21$Hatum21$
 
+@Test
+    public  void loginTest(){
+    new LoginPage(driver)
+            .login("hatum","21$Hatum21$")
+            .verifyUserName("hatum")
+            .logout();
 
+
+}
 
 }
